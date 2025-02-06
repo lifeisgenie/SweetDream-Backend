aaeaf5c8adb98697843f2ad6d70021560adb4048
lifeisgenie <walker3z@naver.com>
2025-02-06 23:33:52 +0900

    Initial commit with new Sweet_Dream project files

---
+++ src/main/java/com/example/Sweet_Dream/SweetDreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sweet_Dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sweet_Dream/SweetDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sweet_Dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
